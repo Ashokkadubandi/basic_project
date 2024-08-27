95cd4cf20d5caf0567365d83e1ecc3189c8d2d5c
Ashok <velmaashok765@gmail.com>
2024-08-27 06:52:41 +0000

    update mobile navbar

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let navBtn = document.getElementById("open");
+let closeBtn = document.getElementById("close");
+let mobileNav = document.getElementById("hide");
 
+navBtn.onclick = function () {
+  mobileNav.classList.add("move-right");
+};
+closeBtn.onclick = function () {
+  mobileNav.classList.toggle("move-right");
+};
